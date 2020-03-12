d5c0b0e9c1de1dd5523ac930a12a2863e724f290
baket432 <62102401+baket432@users.noreply.github.com>
2020-03-12 18:15:41 +0300

    Add files via upload

---
+++ num.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace num
+{
+    public class num
+    {
+        public bool qwe(string x)
+        {
+            string letter = "АВЕКМНОРСТУХ";
+            string chislo = "123456789";
+            int[] region = Enumerable.Range(01, 99).ToArray();
+
+            if ((x.Length == 8 || x.Length == 9) && chislo.Contains(x[1]) && chislo.Contains(x[2]) && chislo.Contains(x[3]) && letter.Contains(x[0]) && letter.Contains(x[4]) && letter.Contains(x[5]) && region.Contains(x[6]) && region.Contains(x[7]))
+            {
+                return true;
+            }
+            else if (((x.Length == 8 || x.Length == 9) && chislo.Contains(x[1]) && chislo.Contains(x[2]) && chislo.Contains(x[3]) && letter.Contains(x[0]) && letter.Contains(x[4]) && letter.Contains(x[5]) && region.Contains(x[6]) && region.Contains(x[7]) && region.Contains(x[8])))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ numTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace num.Test
+{
+    [TestClass]
+    public class numTest
+    {
+        [TestMethod]
+        public void SumTest()
+        {
+            string x = "А321УЕ76";
+
+            bool expected = true;
+
+            num a = new num();
+            bool actual = a.qwe(x);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
